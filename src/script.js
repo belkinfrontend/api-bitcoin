bc165cd5c8240ad40b303486407bbb8a9b3e4f50
Dmitriy Belkin <belkin.frontend@gmail.com>
2018-06-04 01:24:09 +0300

    add chart.js

--- src/script.js
+++ src/script.js
@@ -1,10 +1,9 @@
-$(document).ready(function() {
+$(document).ready(function () {
     //getRate();
     //getHistoryRate();
     $('input[type="date"]').on('input', getHistoryRate);
-    
-});
 
+});
 
 //let getRate = () => {
 //    
@@ -17,21 +16,44 @@ $(document).ready(function() {
 //    );
 //}
 
-
 function getHistoryRate() {
-    
+
     $.get(
-        "https://api.coindesk.com/v1/bpi/historical/close.json",
-        {
-            "start" : $('#date1').val(),
-            "end" : $('#date2').val()
+        "https://api.coindesk.com/v1/bpi/historical/close.json", {
+            "start": $('#date1').val(),
+            "end": $('#date2').val()
         },
-        function(data) {
+        function (data) {
             data = JSON.parse(data);
             console.log(data.bpi);
-//            document.getElementById("out").innerHTML = JSON.stringify(data.bpi);
+            //document.getElementById("out").innerHTML = JSON.stringify(data.bpi);
             document.getElementById("out").innerHTML = "go to console =>";
-            
+
+
+
+            let ctx = document.getElementById('myChart').getContext('2d');
+            let chart = new Chart(ctx, {
+                // The type of chart we want to create
+                type: 'line',
+
+                // The data for our dataset
+                data: {
+                    labels: ["January", "February", "March", "April", "May", "June", "July"],
+                    datasets: [{
+                        label: "My First dataset",
+                        backgroundColor: 'rgb(255, 99, 132)',
+                        borderColor: 'rgb(255, 99, 132)',
+                        data: [0, 10, 5, 2, 20, 30, 45],
+                    }]
+                },
+
+                // Configuration options go here
+                options: {}
+            });
+
         }
     );
+
+
+
 }
